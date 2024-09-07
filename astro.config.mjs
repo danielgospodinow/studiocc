c5f176fbf2510a2ea2d7064898b1f54796c50842
danielgospodinow <danielgospodinow@gmail.com>
2024-09-07 19:46:40 +0000

    Move stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify()
+  integrations: [
+    tailwind()
+  ]
 });
